1f813d2408dc7987e2c4225fd4d2c7ba61e0dc89
Yuan Wu <wu.yuanf@gmail.com>
2024-03-19 17:59:42 -0400

    fixed: auth middleware

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -23,7 +23,7 @@ export const createPost = async(req,res) =>{
         res.status(400).json({error:'All fields are required'})
     }
     try {
-        const newPost = await Post.create({title,content})
+        const newPost = await Post.create({title,content,user:req.user})
         console.log(newPost)
         
         res.status(200).json({success:"Post created",title,content})
--- api/utils/auth.js
+++ api/utils/auth.js
@@ -3,16 +3,19 @@ import User from '../models/user.model.js'
 
 
 export const auth = async(req,res,next)=>{
-    const {authorization} = req.headers 
+    const {authorization} = req.headers
+    // console.log('req.headers info:',req.headers) 
     if(!authorization){
         return res.status(401).json({error:'Authorization token not found'})
     }
 
-    const token = authorization.split(" ")[1]
-    console.log(token)
+    const token = authorization?.split(" ")[1]
+    // console.log(token)
     try {
         const {_id} = jwt.verify(token, process.env.JWT_SECRET)
         req.user = await User.findById(_id).select('_id')
+        // console.log("req.user info:",req.user)
+
 
         next()
     } catch (error) {
